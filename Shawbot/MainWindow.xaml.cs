0907109616f375cc71f366c9b52dc92b011c3db1
Michael Zanussi <iosdevx@gmail.com>
2017-02-08 21:22:23 -0700

    UI updates

--- Shawbot/MainWindow.xaml.cs
+++ Shawbot/MainWindow.xaml.cs
@@ -20,9 +20,64 @@ namespace Shawbot
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string timestamp = "";
+
         public MainWindow()
         {
             InitializeComponent();
+            btnStart.Focus();
+            int filenumber = Shawbot.Properties.Settings.Default.Fileno;
+            int lastline = Shawbot.Properties.Settings.Default.Lastline;
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = false;
+            btnStop.IsEnabled = true;
+            btnPause.IsEnabled = true;
+            btnPause.Focus();
+            btnReset.IsEnabled = false;
+            ckNoTweet.IsEnabled = false;
+            timestamp = DateTime.Now.ToString();
+            lblTimestamp.Content = "(running since " + timestamp + ")";
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+            btnPause.IsEnabled = false;
+            btnReset.IsEnabled = true;
+            ckNoTweet.IsEnabled = true;
+            lblTimestamp.Content = "";
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = false;
+            btnStop.IsEnabled = true;
+            btnPause.IsEnabled = true;
+            btnReset.IsEnabled = false;
+            ckNoTweet.IsEnabled = false;
+            string content = (string)btnPause.Content;
+            if ("Pause".Equals(content))
+            {
+                lblTimestamp.Content = "(paused)";
+                btnPause.Content = "Resume";
+            }
+            else
+            {
+                lblTimestamp.Content = timestamp;
+                btnPause.Content = "Pause";
+            }
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            // reset parameters (current file # to 1, current line # to 1)
+            Shawbot.Properties.Settings.Default.Fileno = 0;
+            Shawbot.Properties.Settings.Default.Lastline = 0;
+            Shawbot.Properties.Settings.Default.Save();
         }
     }
 }
--- Shawbot/Properties/Settings.Designer.cs
+++ Shawbot/Properties/Settings.Designer.cs
@@ -8,22 +8,42 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Shawbot.Properties
-{
-
-
+namespace Shawbot.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int Fileno {
+            get {
+                return ((int)(this["Fileno"]));
+            }
+            set {
+                this["Fileno"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int Lastline {
+            get {
+                return ((int)(this["Lastline"]));
+            }
+            set {
+                this["Lastline"] = value;
             }
         }
     }
